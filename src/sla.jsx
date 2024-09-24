d3b6c23412c3a71f1683eefcfd37e212d5995fe6
ClaytonRochaJr <claytonrocha179@gmail.com>
2024-09-24 20:54:36 -0300

    alterando o codigo v3

--- src/sla.jsx
+++ src/sla.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
 function MyComponent() {
-    return <h1>Hello, world! <p>Boa noite</p> <p>Boa tarde</p></h1>;
+    return <h1>Hello, world! <p>Boa noite</p> <p>Boa tarde</p> <p>Bom dia</p></h1>;
   }
   
